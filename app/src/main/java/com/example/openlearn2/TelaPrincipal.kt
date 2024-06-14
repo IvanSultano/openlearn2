76d884f2bc569ea83af71cd197802c1bc1f7e935
Estudos <ivan.domingues@hotmail.com>
2024-06-13 22:14:29 -0300

    NavBar

--- app/src/main/java/com/example/openlearn2/TelaPrincipal.kt
+++ app/src/main/java/com/example/openlearn2/TelaPrincipal.kt
@@ -3,13 +3,48 @@ package com.example.openlearn2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.openlearn2.databinding.ActivityTelaPrincipalBinding
 
 class TelaPrincipal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_principal)
+        binding = ActivityTelaPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
         supportActionBar?.hide()
         window.statusBarColor = Color.parseColor("#FFFF9800")
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.settings -> replaceFragment(Settings())
+
+                else ->{
+
+
+
+
+                }
+            }
+
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
